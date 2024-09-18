106946dfcf1e52ebad26d1f6050bb83892d2e713
sanya1111111111 <116189775+sanya1111111111@users.noreply.github.com>
2024-09-18 14:55:13 +0300

    Добавлены коментарии

--- asciify.py
+++ asciify.py
@@ -1,42 +1,25 @@
 from PIL import Image
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
+ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']  # Символы необходимые для формирования изображения
 ASCII_CHARS = ASCII_CHARS[::-1]
 
-'''
-method resize():
-    - takes as parameters the image, and the final width
-    - resizes the image into the final width while maintaining aspect ratio
-'''
-def resize(image, new_width=100):
+def resize(image, new_width=100):  # Изменяет размер изображения
     (old_width, old_height) = image.size
     aspect_ratio = float(old_height)/float(old_width)
     new_height = int(aspect_ratio * new_width)
     new_dim = (new_width, new_height)
     new_image = image.resize(new_dim)
     return new_image
-'''
-method grayscalify():
-    - takes an image as a parameter
-    - returns the grayscale version of image
-'''
-def grayscalify(image):
+
+def grayscalify(image):  # Делает изображение чёрно-белым
     return image.convert('L')
 
-'''
-method modify():
-    - replaces every pixel with a character whose intensity is similar
-'''
-def modify(image, buckets=25):
+def modify(image, buckets=25):  # Заменяет пиксели на символы схожей интенсивности
     initial_pixels = list(image.getdata())
     new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
     return ''.join(new_pixels)
 
-'''
-method do():
-    - does all the work by calling all the above functions
-'''
-def do(image, new_width=100):
+def do(image, new_width=100):  # Вызывает ранее описанные функции
     image = resize(image)
     image = grayscalify(image)
 
@@ -48,13 +31,7 @@ def do(image, new_width=100):
 
     return '\n'.join(new_image)
 
-'''
-method runner():
-    - takes as parameter the image path and runs the above code
-    - handles exceptions as well
-    - provides alternative output options
-'''
-def runner(path):
+def runner(path):  # Управляет процессом выполнения программы и обрабатывает исключения
     image = None
     try:
         image = Image.open(path)
@@ -75,15 +52,10 @@ def runner(path):
     f.write(image)
     f.close()
 
-'''
-method main():
-    - reads input from console
-    - profit
-'''
-if __name__ == '__main__':
+if __name__ == '__main__':  # Вход в программу, здесь происходит определение идёт работа с локальным файлом или интернет ресурсом
     import sys
     import urllib.request
-    if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
+    if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):  # Если параметр содержит ссылку программа скачивает обрабатываемый файл
         urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
         path = "asciify.jpg"
     else:
